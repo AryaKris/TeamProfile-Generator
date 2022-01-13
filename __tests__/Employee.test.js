15f1dddc31bdce18bd9b0bfe2d2e437e3f743fd4
Arya Krishna <dr.aryakris@outlook.com>
2022-01-12 18:09:49 -0800

    jest install

---
+++ __tests__/Employee.test.js
@@ -0,0 +1,36 @@
+const Employee = require('../lib/Employee');
+//organize
+describe("Employee",() =>{
+
+    //Methods
+    describe ("Initialization/Constructor", () =>{
+        //Testing of object properties after constructing a new instance
+        it("should build with prop x when give y", ()=>{
+
+        })
+    });
+
+    describe("getName", () =>{
+        //Arrange
+        //Create a new Employee object
+
+        //Act
+        //Call the getName meathod and store the result
+
+        //Assert
+        //The result is equal to an expected value
+
+    });
+
+    describe("getId", () => {
+
+    });
+
+    describe("getEmail", () => {
+
+    });
+
+    describe("getName", () => {
+
+    });
+});
