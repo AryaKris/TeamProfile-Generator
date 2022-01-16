408fbefa29a59de6b4d6f8fb6792b5a5f1a75085
Arya Krishna <dr.aryakris@outlook.com>
2022-01-16 00:00:20 -0800

    using a switch case to add more prompts

--- index.js
+++ index.js
@@ -19,23 +19,29 @@ inquirer.prompt ([
     type: 'input',
     message: 'What is your name?',
     name: 'name'   
+    },
+    
+    {
+        type: 'input',
+        message: 'What is your email?',
+        name: 'email'
+    },  
+    {
+        type: 'input',
+        message: 'What is your id?',
+        name: 'id'
+    },
+    {
+        type: 'input',
+        message: 'What is your office number?',
+        name: 'officeNumber'
+    },
+    {
+        type: 'list',
+        name: 'addMember',
+        message: 'What type of team member would you like to add?',
+        choices: ['Engineer', 'Intern', 'I don\'t want to add any more team members'],
     }
-    // },
-    // {
-    //     type: 'input',
-    //     message: 'What is your email?',
-    //     name: 'email'
-    // },  
-    // {
-    //     type: 'input',
-    //     message: 'What is your id?',
-    //     name: 'id'
-    // },
-    // {
-    //     type: 'input',
-    //     message: 'What is your office number?',
-    //     name: 'officeNumber'
-    // }
     
 ])
 .then (function(data){
@@ -43,27 +49,37 @@ inquirer.prompt ([
     const manager = new Manager(data.name, data.id, data.email, data.officeNumber);
     employees.push(manager);
     console.log('Manager: ',manager);
+    switch(data.addMember){
+        case 'Engineer':
+            engineerQuestions();
+            break;
+            case 'Intern':
+                internQuestions();
+                break;
+                default:
+
+    }
 
     //call next prompt function that lets you pick next employee to add using more enquire prompts  
-    console.log("existLoop: ", exitLoop);
-    while (exitLoop){
-        inquirer.prompt([
-            {
-                type: 'list',
-                name: 'nextEmployee',
-                message: 'Whom should be added next?',
-                choices: ['Engineer', 'Intern','existLoop'],
-            }
-        ])
-            .then((answers) => {
-                if (answers.nextEmployee != 'existLoop') {
-                    console.info('Answer:', answers.nextEmployee);
-                }
-                else {
-                    break;
-                }                
-            });
-    }    
+//     console.log("exitLoop: ", exitLoop);
+//    while (exitLoop){
+//         inquirer.prompt([
+//             {
+//                 type: 'list',
+//                 name: 'nextEmployee',
+//                 message: 'Whom should be added next?',
+//                 choices: ['Engineer', 'Intern','exitLoop'],
+//             }
+//         ])
+//             .then((answers) => {
+//                 if (answers.nextEmployee != 'exitLoop') {
+//                     console.info('Answer:', answers.nextEmployee);
+//                 }
+//                 else {
+//                     return;
+//                 }                
+//             });
+//     }    
 });
 
 
