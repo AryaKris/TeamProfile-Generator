ea2fcecaa8becb35f332899f466f0f5a739f5652
Arya Krishna <dr.aryakris@outlook.com>
2022-01-16 23:48:48 -0800

    prompt for inern and engineer, debugging

--- index.js
+++ index.js
@@ -2,10 +2,10 @@
 
 const inquirer = require("inquirer");
 const fs = require("fs");
-const Manager = require ('./lib/Manager');
+const Manager = require('./lib/Manager');
 const Engineer = require('./lib/Engineer');
 const Intern = require('./lib/Intern');
-
+const Employee = require("./lib/Employee");
 
 //Ask them for manager info
 
@@ -13,97 +13,158 @@ const employees = [];
 const exitLoop = true;
 
 //prompt the user for the data for Manager
+init();
 
-inquirer.prompt ([
-    {
-    type: 'input',
-    message: 'What is your name?',
-    name: 'name'   
-    },
-    
-    {
-        type: 'input',
-        message: 'What is your email?',
-        name: 'email'
-    },  
-    {
-        type: 'input',
-        message: 'What is your id?',
-        name: 'id'
-    },
-    {
-        type: 'input',
-        message: 'What is your office number?',
-        name: 'officeNumber'
-    },
-    {
-        type: 'list',
-        name: 'addMember',
-        message: 'What type of team member would you like to add?',
-        choices: ['Engineer', 'Intern', 'I don\'t want to add any more team members'],
-    }
-    
-])
-.then (function(data){
-    console.log(data);
-    const manager = new Manager(data.name, data.id, data.email, data.officeNumber);
-    employees.push(manager);
-    console.log('Manager: ',manager);
-    switch(data.addMember){
-        case 'Engineer':
-            engineerQuestions();
-            break;
-            case 'Intern':
-                internQuestions();
-                break;
+function init(){
+    inquirer.prompt([
+        {
+            type: 'input',
+            message: 'What is your name?',
+            name: 'name'
+        },
+        {
+            type: 'input',
+            message: 'What is your email?',
+            name: 'email'
+        },
+        {
+            type: 'input',
+            message: 'What is your id?',
+            name: 'id'
+        },
+        {
+            type: 'input',
+            message: 'What is your office number?',
+            name: 'officeNumber'
+        },
+        {
+            type: 'list',
+            name: 'addMember',
+            message: 'What type of team member would you like to add?',
+            choices: ['Engineer', 'Intern', 'I don\'t want to add any more team members'],
+        }
+    ])
+        .then(function (data) {
+            console.log("Comment1");
+            const manager = new Manager(data.name, data.id, data.email, data.officeNumber);
+            employees.push(new Employee(manager));
+            // employees.push(manager);
+            console.log('Manager: ', manager);
+            switch (data.addMember) {
+                case 'Engineer':
+                    engineerQuestions();
+                    break;
+                case 'Intern':
+                    internQuestions();
+                    break;
                 default:
 
-    }
+            }
+        });
+}
 
-    //call next prompt function that lets you pick next employee to add using more enquire prompts  
-//     console.log("exitLoop: ", exitLoop);
-//    while (exitLoop){
-//         inquirer.prompt([
-//             {
-//                 type: 'list',
-//                 name: 'nextEmployee',
-//                 message: 'Whom should be added next?',
-//                 choices: ['Engineer', 'Intern','exitLoop'],
-//             }
-//         ])
-//             .then((answers) => {
-//                 if (answers.nextEmployee != 'exitLoop') {
-//                     console.info('Answer:', answers.nextEmployee);
-//                 }
-//                 else {
-//                     return;
-//                 }                
-//             });
-//     }    
-});
+//prompt for engineer questions
+const engineerQuestions = () => {
+    inquirer.prompt([
+        {
+            type: 'input',
+            message: 'What is your name?',
+            name: 'name'
+        },
 
+        {
+            type: 'input',
+            message: 'What is your email?',
+            name: 'email'
+        },
+        {
+            type: 'input',
+            message: 'What is your id?',
+            name: 'id'
+        },
+        {
+            type: 'input',
+            message: 'What is your githubUsername?',
+            name: 'gitHub'
+        },
+        {
+            type: 'list',
+            name: 'addMember',
+            message: 'What type of team member would you like to add?',
+            choices: ['Engineer', 'Intern', 'I don\'t want to add any more team members'],
+        }
+    ])
+        .then(function (engineerData) {
+            console.log("engineer:", engineerData);
+            const engineer = new Engineer(engineerData.name, engineerData.id, engineerData.email, engineerData.gitHub);
+            employees.push(new Employee(engineer));
+            switch (engineerData.addMember) {
+                case 'Engineer':
+                    engineerQuestions();
+                    break;
+                case 'Intern':
+                    internQuestions();
+                    break;
+                default:
+                    employees.forEach(function (arrayItem) {
+                        console.log(arrayItem);
+                    });
+            }
+        });
+        return;
+}
 
-// nextEmployee();
+//prompt for Intern 
+const internQuestions = () => {
+    inquirer.prompt([
+        {
+            type: 'input',
+            message: 'What is your name?',
+            name: 'name'
+        },
 
+        {
+            type: 'input',
+            message: 'What is your email?',
+            name: 'email'
+        },
+        {
+            type: 'input',
+            message: 'What is your id?',
+            name: 'id'
+        },
+        {
+            type: 'input',
+            message: 'What is your schoolName?',
+            name: 'school'
+        },
+        {
+            type: 'list',
+            name: 'addMember',
+            message: 'What type of team member would you like to add?',
+            choices: ['Engineer', 'Intern', 'I don\'t want to add any more team members'],
+        }
+    ])
+        .then(function (internData) {            
+            const intern = new Intern(internData.name, internData.id, internData.email, internData.school);
+            const employee = new Employee(intern);
+            employees.push(new Employee(intern));
+            console.log("intern:", employee);
+            switch (internData.addMember) {
+                case 'Engineer':
+                    engineerQuestions();
+                    break;
+                case 'Intern':
+                    internQuestions();
+                    break;
+                default:
+                    employees.forEach(function (arrayItem) {
+                        console.log(arrayItem);
+                    });
+            }
+        });
+}
 
-
-
-
-
- //THEN create and store an object for the manager 
-
- //THEN `ask what they want to do next'
- 
-
-//Ask them for engineer info
-
-// Ask them for intern info
-
-
-// Ask them what they would like to do next
-
- // Add employee, and intern, Be done
-//If they want to be done, use all of the collected employee data to build an HTML 
-
-
+// Add employee, and intern, Be done
+//If they want to be done, use all of the collected employee data to build an HTML
 
